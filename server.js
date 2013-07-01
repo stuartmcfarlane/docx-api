e835185fb7baabe14b3a07275c8eb3cbea0463d0
Stuart McFarlane <stuart@mcfarlane.nl>
2013-07-01 18:07:10 +0200

    using docx stream transform

--- server.js
+++ server.js
@@ -16,36 +16,45 @@ server.configure(function(){
     server.use(server.router);
 });
 
+function error404(err, req, res) {
+  res.render('404.jade', { locals: { 
+            title : '404 - Not Found'
+           ,description: ''
+           ,author: ''
+           ,analyticssiteid: 'XXXXXXX' 
+          },status: 404 });
+};
+
+function error500(err, req, res) {
+  res.render('500.jade', { locals: { 
+            title : 'The Server Encountered an Error'
+           ,description: ''
+           ,author: ''
+           ,analyticssiteid: 'XXXXXXX'
+           ,error: err 
+          },status: 500 });
+};
+
+
 //setup the errors
 server.error(function(err, req, res, next){
     if (err instanceof NotFound) {
-        res.render('404.jade', { locals: { 
-                  title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
+      error404(err, req, res);
     } else {
-        res.render('500.jade', { locals: { 
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
+      error500(err, req, res);
     }
 });
 server.listen( port);
 
 //Setup Socket.IO
 var io = io.listen(server);
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function onSocketConnect(socket){
   console.log('Client Connected');
-  socket.on('message', function(data){
+  socket.on('message', function onSocketMessage(data){
     socket.broadcast.emit('server_message',data);
     socket.emit('server_message',data);
   });
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function onSocketDisconnect(){
     console.log('Client Disconnected.');
   });
 });
@@ -57,7 +66,7 @@ io.sockets.on('connection', function(socket){
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
-server.get('/', function(req,res){
+server.get('/', function getIndex(req,res){
   res.render('index.jade', {
     locals : { 
               title : 'Your Page Title'
@@ -68,14 +77,26 @@ server.get('/', function(req,res){
   });
 });
 
+///////////////////////////////////////////
+//              API routes               //
+///////////////////////////////////////////
+
+var request = require('request');
+var docx = require('docx-transform');
+
+server.get('/api/v1/docx', function apiV1Docx (req, res) {
+  var url = req.query.url;
+  console.log('url', url);
+  request.get(url).pipe(docx.toDocx).pipe(res);
+});
 
 //A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
+server.get('/500', function get500(req, res){
     throw new Error('This is a 500 Error');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
+server.get('/*', function get400(req, res){
     throw new NotFound;
 });
 
